bd303b5bd83dabe6c7e5da3d4738a12927af1990
nohsangwoo <nsgr12@naver.com>
2025-05-17 12:18:50 +0900

    feaT: 개선

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type React from "react"
-import type { Metadata } from "next/dist/lib/metadata/types/metadata-interface"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from 'react'
+import type { Metadata } from 'next/dist/lib/metadata/types/metadata-interface'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import { ThemeProvider } from '@/components/theme-provider'
 // import AdminFloatingButton from "@/components/admin-floating-button"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "전교1등 공부습관캠프 전일공",
-  description: "의대생 공부습관 연구소가 제공하는 전교1등 공부습관 캠프입니다.",
-    generator: 'v0.dev'
+  title: '전교1등 공부습관캠프 전일공',
+  description: '의대생 공부습관 연구소가 제공하는 전교1등 공부습관 캠프입니다.',
+  generator: 'ludgi.dev',
 }
 
 export default function RootLayout({
@@ -23,7 +23,12 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {/* <AdminFloatingButton /> */}
           <div className="flex min-h-screen flex-col">
             <Header />
